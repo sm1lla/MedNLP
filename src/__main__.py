db8df8032c95a361a9bc41668ac9bf062f4b5530
martin.preiss <martinpreiss524@gmail.com>
2023-05-10 15:51:39 +0200

    kickoff
    Co-authored-by: Smilla Fox <sm1lla@users.noreply.github.com>

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+import sys
+import pathlib
+
+
+sys.path.append(str(pathlib.Path().resolve()))
+
+from src.cli import cli  # noqa: E402
+
+if __name__ == "__main__":
+    print(pathlib.Path().resolve())
+    cli(prog_name="mednlp")
+
