f7182300e4eb4e00e65ad9510d33bacef0fbb107
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-05-10 16:46:46 +0200

    WIP: Move code from jupyter notebook

--- src/cli.py
+++ src/cli.py
@@ -2,15 +2,29 @@ import typer
 
 cli = typer.Typer(name="TODO: find name")
 
+
 @cli.command()
 def moin():
     print("moin")
 
+
 @cli.command()
 def test():
     from .train import train
+
     train()
+
 
 @cli.command()
 def main(output_file_name):
-    
+    pass
+
+@cli.command()
+def dataset():
+    from .dataset import examine_dataset
+    examine_dataset()
+
+@cli.command()
+def train():
+    from .train import train
+    train()
