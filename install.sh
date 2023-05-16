73e0a079793843bca2cb706151aa37261b2f4a7d
martin.preiss <martin.preiss@student.hpi.uni-potsdam.de>
2023-05-16 10:52:39 +0200

    install stuff

---
+++ install.sh
@@ -0,0 +1,10 @@
+conda install pytorch  pytorch-cuda=11.7 -c pytorch -c nvidia
+conda install -c anaconda pandas= 2.0.1 -y
+conda install -c conda-forge transformers  -y
+conda install scikit-learn -y 
+conda install -c conda-forge matplotlib -y
+conda install -c conda-forge hydra-core -y 
+conda install -c conda-forge black -y
+conda install -c conda-forge isort -y
+pip install --upgrade accelerate
+pip install wandb
