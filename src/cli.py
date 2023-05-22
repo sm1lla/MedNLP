b05b0fa8798371142015ecaf26ea70e521be9df6
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-05-22 15:31:05 +0200

    each symptom metrics sklearn

--- src/cli.py
+++ src/cli.py
@@ -2,12 +2,18 @@ import hydra
 from omegaconf import DictConfig
 
 from .dataset import examine_dataset
-from .inference import infer
 from .evaluate import evaluate_model
+from .inference import infer
 from .train import train
+
 
 @hydra.main(config_path="config", config_name="config")
 def cli(cfg: DictConfig):
-    options = {"train": train, "infer": infer, "dataset": examine_dataset , "evaluate": evaluate_model}
+    options = {
+        "train": train,
+        "infer": infer,
+        "dataset": examine_dataset,
+        "evaluate": evaluate_model,
+    }
 
     options[cfg.task.name](cfg)
