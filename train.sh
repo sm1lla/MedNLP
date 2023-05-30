7e8c328d21cc179e8af787a21ff37bf91f18bbc5
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-05-30 10:04:04 +0200

    wandb sweep with config file

--- train.sh
+++ train.sh
@@ -1,17 +1,16 @@
 #!/bin/bash -eux
-#SBATCH --job-name=trocrPartitions
+#SBATCH --job-name=sweep
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=martin.preiss@student.hpi.uni-potsdam.de
-#SBATCH --partition=gpupro # -p
+#SBATCH --mail-user=smilla.fox@mattermost
+#SBATCH --partition=gpu # -p
 #SBATCH --gpus=1
-#SBATCH --output=trocrPartitions2kto10k.out
 
  
 # Initialize conda:
 eval "$(conda shell.bash hook)"
 set +eu
-conda activate mlens
+conda activate mednlp
 set +eu
 
 
-python -m src train
+wandb agent mednlp/MedNLP/irejz2g6
