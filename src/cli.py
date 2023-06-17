e747f56a4dd8c12a5487ccb6d1b1e1a7c7bdd03a
martin.preiss <martin.preiss@student.hpi.uni-potsdam.de>
2023-06-17 10:27:49 +0200

    first ensemble (happyface) added kfold technique estimator class wrapper for huggingface-models classifier class ensemble config ensemble cli

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,12 @@
 import hydra
 from omegaconf import DictConfig
 
-
-from .debug import debug
 from .dataset import examine_dataset, print_examples_for_classnames
+from .debug import debug
 from .evaluate import evaluate_model
 from .inference import infer
 from .predict import predict
+from .start_ensemble import start_ensemble
 from .train import train
 
 
@@ -19,7 +19,8 @@ def cli(cfg: DictConfig):
         "dataset": examine_dataset,
         "evaluate": evaluate_model,
         "debug": debug,
-        "examples": print_examples_for_classnames
+        "ensemble": start_ensemble,
+        "examples": print_examples_for_classnames,
     }
 
     options[cfg.task.name](cfg)
