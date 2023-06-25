56533fd3ce9a99267280b82e25ccc4f951a4275c
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-06-25 11:34:58 +0200

    generate tweets with api

--- src/cli.py
+++ src/cli.py
@@ -8,6 +8,7 @@ from .inference import infer
 from .predict import predict
 from .start_ensemble import start_ensemble
 from .train import train
+from .tweet_generation import generate_for_all_classes
 
 
 @hydra.main(config_path="config", config_name="config")
@@ -21,6 +22,7 @@ def cli(cfg: DictConfig):
         "debug": debug,
         "ensemble": start_ensemble,
         "examples": print_examples_for_classnames,
+        "generate": generate_for_all_classes,
     }
 
     options[cfg.task.name](cfg)
