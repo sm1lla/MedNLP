d2fb8ff5e2b3547b7bb00a472d794104659af765
smilla.fox <smilla.fox@student.hpi.uni-potsdam.de>
2023-07-11 16:36:06 +0200

    actually evaluate on test set

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,22 +1,29 @@
+from datasets import DatasetDict
 from omegaconf import DictConfig
 
-from .utils import configure_wandb
-from .train import initialize_trainer,init_trainer_with_dataset
-from datasets import DatasetDict
+import wandb
 
-def evaluate_model(cfg: DictConfig,use_test:bool=False):
+from .train import init_trainer_with_dataset, initialize_trainer
+from .utils import add_section_to_metric_log, configure_wandb
+
+
+def evaluate_model(cfg: DictConfig, use_test: bool = True):
     if use_test:
         configure_wandb(cfg)
-    trainer = initialize_trainer(cfg,use_test)
+    trainer = initialize_trainer(cfg, use_test)
+    test_evaluation = trainer.evaluate(trainer.eval_dataset)
+    if use_test:
+        wandb.log(add_section_to_metric_log("test", test_evaluation, "eval_"))
 
-    results = trainer.evaluate()
+    return test_evaluation
 
-    return results
 
-def evaluate_model_with_data(cfg: DictConfig,dataset:DatasetDict,use_test:bool=False):
+def evaluate_model_with_data(
+    cfg: DictConfig, dataset: DatasetDict, use_test: bool = False
+):
     if use_test:
         configure_wandb(cfg)
-    trainer = init_trainer_with_dataset(cfg=cfg,dataset=dataset,use_test=use_test)
+    trainer = init_trainer_with_dataset(cfg=cfg, dataset=dataset, use_test=use_test)
 
     results = trainer.evaluate()
 
